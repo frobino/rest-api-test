4ac0bd162faccd32416d82eb0c1cbb60c7f8fcb1
frobino <frobino@users.noreply.github.com>
2021-01-08 18:53:08 +0100

    [jaxrs-js] add comments in server to improve toggle functionality

--- java/server/jaxrs-test-app/src/io/github/frobino/app/TodoResource.java
+++ java/server/jaxrs-test-app/src/io/github/frobino/app/TodoResource.java
@@ -41,6 +41,12 @@ public class TodoResource {
 
     /*
      * (U) Update (using a post).
+     * 
+     * FIXME: should this be a PUT? It looks like it is idempotent.
+     * I have to learn how to use "HTTP verbs" (i.e. PUT, POST, etc.) better.
+     * 
+     * RESTFUL service essence:
+     * It is all about "create resources and apply the HTTP verbs against it".
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -48,9 +54,17 @@ public class TodoResource {
     	
     	int updatedId = c.getId();
     	String updatedText = c.getText();
-    	// FIXME: as shortcut I am using the complete param to differentiate
-    	// between updating whole todo, or if to just toggle the complete flag.
-    	// Separate end point or QueryParam would be a better option.
+    	/* FIXME: as shortcut I am using the complete param to differentiate
+    	 * between updating whole todo, or if to just toggle the complete flag.
+    	 *  
+    	 * Better options:
+    	 * - Toggle is not a restful action, so PUT cannot be used.
+    	 *   Separate end point (create the "toggle todo resource"),
+    	 *   e.g. POST model/toggle/1
+    	 *   or PUT/POST/DELETE/GET model/1/toggled
+    	 * - create a separate PATCH [{"op": "toggle"}, {"op": "editText", "id": <id>, "text": "updatedText"}]
+    	 * - use QueryParam?
+    	 */
     	boolean toggleComplete = c.getComplete();
     	
     	boolean originalCompleteFlag = TodoDao.instance.getModel().get(updatedId).getComplete();
